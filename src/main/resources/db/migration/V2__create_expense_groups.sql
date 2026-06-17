88ec7e943fbad00e649bd2fdae918c65f05e9520
Talha <17671511+iTzTalha@users.noreply.github.com>
2026-06-18 02:33:53 +0530

    remove is_active column

--- src/main/resources/db/migration/V2__create_expense_groups.sql
+++ src/main/resources/db/migration/V2__create_expense_groups.sql
@@ -2,14 +2,12 @@ CREATE TABLE expense_groups
 (
     id               UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    whatsapp_chat_id TEXT    NOT NULL,
+    whatsapp_chat_id TEXT NOT NULL,
 
-    name             TEXT    NOT NULL,
+    name             TEXT NOT NULL,
     description      TEXT,
 
-    created_by_wa_id TEXT    NOT NULL,
-
-    is_active        BOOLEAN NOT NULL,
+    created_by_wa_id TEXT NOT NULL,
 
     CONSTRAINT uq_expense_group_name UNIQUE (whatsapp_chat_id, name)
 );
