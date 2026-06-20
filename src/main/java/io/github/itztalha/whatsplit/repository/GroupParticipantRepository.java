b54b12cb7e9fbd3285f852fb77ed935b307c4049
Talha <17671511+iTzTalha@users.noreply.github.com>
2026-06-21 02:02:57 +0530

    Update Group Participant repository

--- src/main/java/io/github/itztalha/whatsplit/repository/GroupParticipantRepository.java
+++ src/main/java/io/github/itztalha/whatsplit/repository/GroupParticipantRepository.java
@@ -1,7 +1,21 @@
 package io.github.itztalha.whatsplit.repository;
 
-import org.springframework.stereotype.Repository;
+import io.github.itztalha.whatsplit.model.common.WaId;
+import io.github.itztalha.whatsplit.model.group.GroupParticipant;
 
-@Repository
-public class GroupParticipantRepository {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface GroupParticipantRepository {
+
+    GroupParticipant save(GroupParticipant participant);
+
+    Optional<GroupParticipant> findByGroupIdAndWaId(UUID expenseGroupId, WaId waId);
+
+    List<GroupParticipant> findByGroupId(UUID expenseGroupId);
+
+    List<GroupParticipant> findActiveByGroupId(UUID expenseGroupId);
+
+    boolean isActiveParticipant(UUID expenseGroupId, WaId waId);
 }
