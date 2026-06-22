4cc69ea75528d6e9af47dc20620527961cb21368
Talha <17671511+iTzTalha@users.noreply.github.com>
2026-06-23 03:28:57 +0530

    Add findById to settlement repository

--- src/main/java/io/github/itztalha/whatsplit/repository/SettlementRepository.java
+++ src/main/java/io/github/itztalha/whatsplit/repository/SettlementRepository.java
@@ -3,11 +3,14 @@ package io.github.itztalha.whatsplit.repository;
 import io.github.itztalha.whatsplit.model.settlement.Settlement;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SettlementRepository {
 
     Settlement save(Settlement settlement);
+
+    Optional<Settlement> findById(UUID settlementId);
 
     List<Settlement> findByGroupId(UUID expenseGroupId);
 }
