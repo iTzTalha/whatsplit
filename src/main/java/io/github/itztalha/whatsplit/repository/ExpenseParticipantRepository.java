5fc9c00c718cc9a0411ce807bf99ec9207654393
Talha <17671511+iTzTalha@users.noreply.github.com>
2026-06-23 01:56:43 +0530

    Update expense participant repository

--- src/main/java/io/github/itztalha/whatsplit/repository/ExpenseParticipantRepository.java
+++ src/main/java/io/github/itztalha/whatsplit/repository/ExpenseParticipantRepository.java
@@ -1,15 +1,19 @@
 package io.github.itztalha.whatsplit.repository;
 
+import io.github.itztalha.whatsplit.model.common.WaId;
 import io.github.itztalha.whatsplit.model.expense.ExpenseParticipant;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ExpenseParticipantRepository {
 
-    List<ExpenseParticipant> saveAll(List<ExpenseParticipant> participants);
+    ExpenseParticipant save(ExpenseParticipant expenseParticipant);
+
+    Optional<ExpenseParticipant> findById(UUID expenseParticipantId);
+
+    Optional<ExpenseParticipant> findByExpenseIdAndWaId( UUID expenseId, WaId waId);
 
     List<ExpenseParticipant> findByExpenseId(UUID expenseId);
-
-    void deleteByExpenseId(UUID expenseId);
 }
